c309189af1e7cac320d387867c76dbe961e7dc66
cathylee12 <kyungeunlee1112@gmail.com>
2022-06-17 22:28:56 -0700

    Added general APIs

--- server/controllers/flashcard.js
+++ server/controllers/flashcard.js
@@ -1,7 +1,6 @@
 import Flashcard from "../models/flashcard.js";
 
 export const createCard = async (req, res) => {
-    console.log(req.body);
     const newCard = new Flashcard({
         front: req.body.front,
         back: req.body.back
@@ -14,11 +13,87 @@ export const createCard = async (req, res) => {
     }
 };
 
+export const getCard = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const card = await Flashcard.findById(id);
+        res.status(200).json(card);
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+};
+
 export const getCards = async (req, res) => {
     try {
         const cards = await Flashcard.find();
         res.status(200).json(cards);
     } catch (err) {
-        res.status(404).json({ message: err.message});
+        res.status(404).json({ message: err.message });
+    }
+};
+
+export const getFront = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const card = await Flashcard.findById(id);
+        res.status(200).json(card.front);
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+};
+
+export const getBack = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const card = await Flashcard.findById(id);
+        res.status(200).json(card.back);
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+};
+
+export const deleteCard = async (req, res) => {
+    const {id} = req.params;
+    try {
+        await Flashcard.findByIdAndRemove(id);
+        res.status(200).json({ message: "Card deleted successfully" });
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+};
+
+export const updateFront = async (req, res) => {
+    const {id} = req.params;
+    try {
+        const card = await Flashcard.findById(id);
+        card.front = req.body.front;
+        await card.save();
+        res.status(200).json(card)
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+};
+
+export const updateBack = async (req, res) => {
+    const {id} = req.params;
+    try {
+        const card = await Flashcard.findById(id);
+        card.back = req.body.back;
+        await card.save();
+        res.status(200).json(card)
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+};
+
+export const updateRecallability = async (req, res) => {
+    const {id} = req.params;
+    try {
+        const card = await Flashcard.findById(id);
+        card.recallability = req.body.recallability;
+        await card.save();
+        res.status(200).json(card)
+    } catch (err) {
+        res.status(404).json({ message: err.message });
     }
 };
--- server/routes/flashcard.js
+++ server/routes/flashcard.js
@@ -4,6 +4,13 @@ import * as flashcardControllers from "../controllers/flashcard.js";
 const flashcardRouter = express.Router();
 
 flashcardRouter.post("/", flashcardControllers.createCard);
+flashcardRouter.get("/:id", flashcardControllers.getCard);
 flashcardRouter.get("/", flashcardControllers.getCards);
+flashcardRouter.get("/:id/front", flashcardControllers.getFront);
+flashcardRouter.get("/:id/back", flashcardControllers.getBack);
+flashcardRouter.delete("/:id", flashcardControllers.deleteCard);
+flashcardRouter.patch("/:id/front", flashcardControllers.updateFront);
+flashcardRouter.patch("/:id/back", flashcardControllers.updateBack);
+flashcardRouter.patch("/:id/recallability", flashcardControllers.updateRecallability);
 
 export default flashcardRouter;
