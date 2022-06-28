ec25992f0213b254fcbb7f3f57856ebfd4a9839d
Aaron <aaron.vmale@gmail.com>
2022-06-27 20:03:27 -0700

    add decks view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
 import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
 import Register from './components/Register';
-import Navbar from "./components/navbar/Navbar";
+import Navbar from "./components/Navbar";
 import SignIn from "./components/SignIn";
+import Decks from "./components/Decks";
 function App() {
   return (
     <>
     <Router>
       <Navbar />
       <Routes>
-        <Route path = '/' element = {<></>} />
+        
         <Route path = '/register' element = {<Register/>} />
         <Route path = '/login' element = {<SignIn/>} />
+        <Route path = '/login' element = {<SignIn/>} />
+        <Route path = '/view-decks' element = {<Decks/>} />
+        
         {/* <Route path = '/addmovie' element = {<AddMovie/>} />
         <Route path = '/login' element = {<Login/>} />
         <Route path = '/register' element = {<Register/>} />
         <Route path = '/moviedetail/:id' element = {<MovieDetail/>} /> */}
       </Routes>
     </Router>
-    hello
+    
     </>
   );
 }
