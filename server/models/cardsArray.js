3cdc002134509ea596e9ee0e1d1a805f97505a21
cathylee12 <kyungeunlee1112@gmail.com>
2022-07-29 23:42:32 -0700

    Added couple unit tests for cards practice, refactored some functions, completed deck delete

--- server/models/cardsArray.js
+++ server/models/cardsArray.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { ObjectId } = require("bson");
 
 const cardsArraySchema = mongoose.Schema({
-  deckID: { type: ObjectId },
+  deckId: { type: ObjectId },
   cards: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Flashcard'}]
 })
 
--- server/routes/deck.js
+++ server/routes/deck.js
@@ -4,8 +4,8 @@ const { authentication } = require("../middleware/authentication.js");
 
 const router = express.Router();
 
-router.post("/push-card",authentication, deckController.pushFlashcard);
-router.post("/remove-card",authentication,deckController.removeFlashcard);
+//router.post("/push-card",authentication, deckController.pushFlashcard);
+//router.post("/remove-card",authentication,deckController.removeFlashcard);
 router.delete("/:id", authentication, deckController.deleteDeck);
 router.post("/", authentication, deckController.createDeck);
 router.get("/", authentication, deckController.getDecks);
@@ -13,7 +13,7 @@ router.get("/:id/practice", authentication, deckController.practiceDeck);
 router.get("/:id/practice/next", authentication, deckController.nextCard);
 // // router.post("/:id", authentication, deckController.addFlashcard);
 // router.get("/:id/cards", authentication, deckController.getDeckFlashcards);
-// router.get("/:id", authentication, deckController.getDeck);
+router.get("/:id", authentication, deckController.getDeck);
 // // router.get("/study/:id", deckController.getStudyDeck);
 // router.patch("/:id", authentication, deckController.updateDeckName);
 
