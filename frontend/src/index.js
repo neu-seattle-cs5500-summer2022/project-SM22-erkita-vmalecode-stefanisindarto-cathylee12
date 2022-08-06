6a9c762f7334b6f68e83bffea29f8f47cf5af060
Aaron <aaron.vmale@gmail.com>
2022-08-05 21:40:41 -0700

    add persistor to allow app to maintain state on page exit or on refresh

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { store } from './app/store';
+import { store, persistor } from './app/store';
 import { Provider } from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+     <PersistGate persistor = {persistor}>
+       <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
